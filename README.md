f80334689754a3096dc0407922f26fa9452c7ef5
HoLo85 <winter@magic.ms>
2020-06-06 18:16:22 +0200

    remodified to keep node backward compatible

--- README.md
+++ README.md
@@ -17,109 +17,19 @@ Configure the IP address, user group and password in the node properties. Unchec
 
 ![Node properties](node-properties.png)
 
-### Configure Input
-
-The node needs a payload to be inserted so it knows which message and what values it should read from the unit.
-
-![Payload config](input-payload.png)
-
-* `id`: tells the node which message contains the values
-* `values`: lists all messages that should be read
-  * `name`: custom name of read value
-  * `divider`: set the divider of the read value
-  * `unit`: unit of read value 
-
-`Message id` and `value ids` can easily be found by connecting to the webinterface and using the browsers developer tools to inspect the values displayed on the `/spotvalues` page.
-
-## Example message payloads
-
-### Input
+## Example Message Payload
 
 ```json
 {
-    "id": "1",
-    "values": {
-        "6100_0046E500": {
-            "name": "phase1_voltage",
-            "divider": 100,
-            "unit": "v"
-        },
-        "6100_0046E600": {
-            "name": "phase2_voltage",
-            "divider": 100,
-            "unit": "v"
-        },
-        "6100_0046E700": {
-            "name": "phase3_voltage",
-            "divider": 100,
-            "unit": "v"
-        },
-        "6100_40463600": {
-            "name": "grid_feedin",
-            "divider": 1,
-            "unit": "W"
-        },
-        "6100_40463700": {
-            "name": "grid_consumption",
-            "divider": 1,
-            "unit": "Wh"
-        },
-        "6100_40263F00": {
-            "name": "power",
-            "divider": 1,
-            "unit": "W"
-        }
-    }
+  "available_sessions": 3,
+  "grid_consumption": 0,
+  "grid_feedin": 6546,
+  "phase1_voltage": 230.12,
+  "phase2_voltage": 231.01,
+  "phase3_voltage": 230.55,
+  "power": 7608
 }
 ```
-
-### Output
-
-```json
-{
-  "phase1_voltage": {
-    "value": 230.00,
-    "unit": "V"
-    },
-  "phase2_voltage": {
-    "value": 230.00,
-    "unit": "V"
-    },
-  "phase3_voltage": {
-    "value": 230.00,
-    "unit": "V"
-    },
-  "grid_feedin": {
-    "value": 350,
-    "unit": "W"
-    },
-  "grid_consumption": {
-    "value": 0,
-    "unit" :"W"
-    },
-  "power": {
-    "value": 3600,
-  "unit": "W"
-  },
-}
-```
-
-## How to find `message` an `value` id
-
-This section shows you how to get the `message` and `value ids` the node needs in the input payload to read values from the devices.
-
-1. connect to the webinterface of you device, i.e. `http(s)://192.168.1.42/#/login`
-2. got to the overview section of your device, i.e. `http(s)://192.168.1.42#/spotvalues`
-3. open your browser developertools
-4. get the `message id` of your device by opening the network tab of the dev tools and selecting the XHR filter
-    * select one of the responses from the `/getValues.json` endpoint
-    ![Find message id](find-message-id.png)
-    * the red is your `message id`, the green a `value id`
-5. get the `value id` of your wanted values by using the inspect function of the dev tools
-    * open on of the accordeons and search for the values you want to read
-    * use the inspection tool to select the displayed value and view the `value id`
-![Find value id](find-value-ids.png)
-      * the value id should be a 13 character long string, in this example it's `6380_40251E00`
 
 ## Login sessions
 In my testings I've ran into problems with the maximum number of sessions the webserver supports (4 in my case). The node creates a session and reuses that until the node is restarted or the session expires. If you're using a reasonable small interval (I'm using 5 seconds) to query the inverter the session normally shouldn't expire. Otherwise a session expires after around 6,5 hours according to my testings. But it may happen that a created session get's lost when the node is stopped ungracefully. Logins to the web interface via a web browser count against the same session limit as well.
@@ -129,4 +39,71 @@ This node tries to collect the metrics very agressively, i. e.:
 * short network timeouts of 1500ms
 * timed out requests are retried after 100ms
 * retries are attempted 3 times
+
+## Configure Custom Messages
+
+The node already provides configurations for sunny boy tripower 8.0/10.0 and storage 2.5 units, but can optionally be configured with an input payload to read custom values from the devices.
+
+![Payload config](input-payload.png)
+
+* `id`: tells the node which message contains the values
+* `values`: lists all messages that should be read
+  * `name`: custom name of read value
+  * `divider`: sets the divider of the read value (optional)
+
+`Message id` and `value ids` can easily be found by connecting to the webinterface and using the browsers developer tools to inspect the values displayed on the `/spotvalues` page.
+
+## Example input payload
+
+### Input
+
+```json
+{
+    "id": "1",
+    "values": {
+        "6100_0046E500": {
+            "name": "phase1_voltage",
+            "divider": 100
+        },
+        "6100_0046E600": {
+            "name": "phase2_voltage",
+            "divider": 100
+        },
+        "6100_0046E700": {
+            "name": "phase3_voltage",
+            "divider": 100
+        },
+        "6100_40463600": {
+            "name": "grid_feedin",
+            "divider": 1
+        },
+        "6100_40463700": {
+            "name": "grid_consumption",
+            "divider": 1
+        },
+        "6100_40263F00": {
+            "name": "power",
+            "divider": 1
+        }
+    }
+}
+```
+
+## How to find `message` an `value` id
+
+This section shows you how to get the `message` and `value ids` the node needs in the input payload to read values from the devices.
+
+1. connect to the webinterface of you device, i.e. `http(s)://192.168.1.42/#/login`
+2. got to the overview section of your device, i.e. `http(s)://192.168.1.42#/spotvalues`
+3. open your browser developer tools
+4. get the `message id` of your device by opening the network tab of the dev tools and selecting the XHR filter
+    * select one of the responses from the `/getValues.json` endpoint
+    ![Find message id](find-message-id.png)
+        * the red is your `message id`, the green a `value id`
+5. get the `value id` of your wanted values by using the inspect function of the dev tools
+    * open one of the accordeons and search for the values you want to read
+    * use the inspection tool to select the displayed value and view the `value id`
+![Find value id](find-value-ids.png)
+      * the value id should be a 13 character long string, in this example it's `6380_40251E00`
+
 
--- sma-webconnect.js
+++ sma-webconnect.js
@@ -1,55 +1,282 @@
 module.exports = function(RED) {
-    const retry = require("requestretry");
+  const retry = require("requestretry");
+  
+  const sb_tripower = {
+    "id": "1",
+    "values": {
+      "6100_0046E500": {
+          "name": "phase1_voltage",
+          "divider": 100
+      },
+      "6100_0046E600": {
+          "name": "phase2_voltage",
+          "divider": 100
+      },
+      "6100_0046E700": {
+          "name": "phase3_voltage",
+          "divider": 100
+      },
+      "6100_40463600": {
+          "name": "grid_feedin",
+          "divider": 1
+      },
+      "6100_40463700": {
+          "name": "grid_consumption",
+          "divider": 1
+      },
+      "6100_40263F00": {
+          "name": "power",
+          "divider": 1
+      }
+    }
+  };
+  
+  const sb_storage = {
+    "id": "7",
+    "values": {
+      "6100_00295A00": {
+          "name": "charge_percent",
+          "divider": 1
+      },
+      "6100_00495C00": {
+          "name": "voltage",
+          "divider": 100
+      },
+      "6400_00496700": {
+          "name": "overall_charge_wh",
+          "divider": 1
+      },
+      "6100_00496900": {
+          "name": "current_charging_w",
+          "divider": 1
+      },
+      "6400_00497E00": {
+          "name": "current_charging_wh",
+          "divider": 1
+      },
+      "6400_00496800": {
+          "name": "overall_discharge_wh",
+          "divider": 1
+      },
+      "6100_00496A00": {
+          "name": "current_discharge_w",
+          "divider": 1
+      },
+      "6400_00496D00": {
+          "name": "current_discharge_wh",
+          "divider": 1
+      },
+      "6400_00496900": {
+          "name": "charge_w",
+          "divider": 1
+      },
+      "6100_40495B00": {
+          "name": "temperature_deg",
+          "divider": 10
+      },
+      "6180_08495E00": {
+          "name": "state_charging",
+          "divider": 1
+      }
+    }
+  };
+  
+  var message = {};
+
+  function request(uri, body, callback) {
+    retry({
+      method: "POST",
+      uri,
+      body,
+      json: true,
+      strictSSL: false,
+      timeout: 1500,
+      maxAttempts: 3,
+      retryDelay: 100
+    }, callback);
+  }
+
+  function buildUrl(https, host, path) {
+    return (https ? "https" : "http") + "://" + host + path;
+  }
+
+  function login(node, callback) {
+    const url = buildUrl(node.use_tls, node.ip_address, "/dyn/login.json");
+
+    node.debug("requesting " + url);
+
+    request(
+      url,
+      {
+        right: node.right,
+        pass: node.credentials.password
+      }, (error, response, body) => {
+        node.debug("response to " + url + ": " + JSON.stringify(response));
+
+        var result;
+
+        if (error) {
+          node.error(error);
+        } else if (response && response.statusCode == 301 && response.headers && response.headers.location) {
+          if (response.headers.location.substring(0, 5) != response.request.uri.protocol) {
+            node.error("detected an redirect to HTTPS, please change your configuration to use HTTPS");
+          }
+        } else if (body) {
+          if (body.err) {
+            node.error(body);
+          } else if (body.result) {
+            result = body.result.sid;
+            node.sid = body.result.sid;
+
+            node.log("session created: " + node.ip_address + " : " + body.result.sid);
+          }
+        }
+
+        if (callback) {
+          callback(result);
+        }
+      }
+    );
+  }
+
+  function getValues(node, callback, onSessionTimeout) {
+    const url = buildUrl(node.use_tls, node.ip_address, "/dyn/getValues.json?sid=" + node.sid);
+      
+    // set default message according to device type
+    if (node.device_selection == "sb_storage") {
+      message = sb_storage;
+    }
+    else if (node.device_selection == "custom") {
+      message = node.custom_message;
+    }
+    else {
+      message = sb_tripower;
+    }
     
-    var message = {};		
-    var _sid = {};		// object to store multiple session keys
+    const value_keys = Object.keys(message.values);
 
-    function request(uri, body, callback) {
-      retry({
-        method: "POST",
-        uri,
-        body,
-        json: true,
-        strictSSL: false,
-        timeout: 1500,
-        maxAttempts: 3,
-        retryDelay: 100
-      }, callback);
-    }
+    node.debug("requesting " + url);
 
-    function buildUrl(https, host, path) {
-      return (https ? "https" : "http") + "://" + host + path;
-    }
+    request(
+      url,
+      {
+        "destDev": [],
+        "keys": value_keys
+      }, (error, response, body) => {
+        node.debug("response to " + url + ": " + JSON.stringify(response));
 
-    function login(node, callback) {
-      const url = buildUrl(node.use_tls, node.ip_address, "/dyn/login.json");
+        if (error) {
+          node.error(error);
+        } else if (body) {
+          if (body.err) {
+            if (body.err == "401") {
+              if (onSessionTimeout) {
+                onSessionTimeout();
+              }
+            } else {
+              node.error(body);
+            }
+          } else if (body.result) {
+            var result = {};
+            
+            // initialize all values to 0
+            for (const key of value_keys) {
+              result[message.values[key].name] = 0;
+            }
 
-      node.debug("requesting " + url);
+            for (const id in body.result) {
+              const set = body.result[id];
+
+              for (const key in set) {
+                if ( message.values.hasOwnProperty(key) ) { 
+                  const value = set[key];
+                  
+                  if (value != null) {
+                    for (const elm of value[message.id]) {
+                      if (elm.val) {
+                        var tmp = null;
+                        
+                        if (elm.val[0]) {
+                          tmp = elm.val[0];
+                        }
+                        else {
+                          if (typeof elm.val === 'number' && message.values[key].divider) {
+                            tmp = elm.val / message.values[key].divider;
+                          }
+                          else {
+                            tmp = elm.val;
+                          }
+                        }
+
+                        result[message.values[key].name] = tmp;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+
+            if (callback) {
+              callback(result);
+            }
+          }
+        }
+      }
+    );
+  }
+
+  function getFreeSessionsCount(node, callback) {
+    const url = buildUrl(node.use_tls, node.ip_address, "/dyn/sessionCheck.json");
+
+    node.debug("requesting " + url);
+
+    request(
+      url,
+      {},
+      (error, response, body) => {
+        node.debug("response to " + url + ": " + JSON.stringify(response));
+
+        if (error) {
+          node.error(error);
+        } else if (body) {
+          if (body.result && body.result.cntFreeSess != null) {
+            if (callback) {
+              callback(body.result.cntFreeSess);
+            }
+          } else {
+            node.log(body);
+          }
+        }
+      }
+    );
+  }
+
+  function logout(node, callback) {
+    if (node.ip_address){
+      const url = buildUrl(node.use_tls, node.ip_address, "/dyn/logout.json?sid=" + node.sid);
+
+      node.log(url);
 
       request(
         url,
-        {
-          right: node.right,
-          pass: node.credentials.password
-        }, (error, response, body) => {
+        {},
+        (error, response, body) => {
           node.debug("response to " + url + ": " + JSON.stringify(response));
 
-          var result;
+          var result = false;
 
           if (error) {
             node.error(error);
-          } else if (response && response.statusCode == 301 && response.headers && response.headers.location) {
-            if (response.headers.location.substring(0, 5) != response.request.uri.protocol) {
-              node.error("detected an redirect to HTTPS, please change your configuration to use HTTPS");
-            }
           } else if (body) {
-            if (body.err) {
-              node.error(body);
-            } else if (body.result) {
-              result = body.result.sid;
-              _sid[node.ip_address] = body.result.sid;
+            if (body.result && body.result.login != null) {
+              if (body.result.isLogin == false) {
+                result = true;
 
-              node.log("session created: " + node.ip_address + " : " + body.result.sid);
+                node.log("session closed: " + node.sid);
+                node.sid = null;
+              }
+            } else {
+              node.log(body);
             }
           }
 
@@ -59,189 +286,60 @@ module.exports = function(RED) {
         }
       );
     }
+  }
 
-    function getValues(node, callback, onSessionTimeout) {
-      const url = buildUrl(node.use_tls, node.ip_address, "/dyn/getValues.json?sid=" + _sid[node.ip_address]);
-      const value_keys = Object.keys(message.values);
+  function query(node, retries, completion) {
+    if (retries > 0) {
+      if (node.sid) {
+        getValues(node, (result) => {
+          getFreeSessionsCount(node, (count) => {
+            result.available_sessions = count;
 
-      node.debug("requesting " + url);
-
-      request(
-        url,
-        {
-          "destDev": [],
-          "keys": value_keys
-        }, (error, response, body) => {
-          node.debug("response to " + url + ": " + JSON.stringify(response));
-
-          if (error) {
-            node.error(error);
-          } else if (body) {
-            if (body.err) {
-              if (body.err == "401") {
-                if (onSessionTimeout) {
-                  onSessionTimeout();
-                }
-              } else {
-                node.error(body);
-              }
-            } else if (body.result) {
-              var result = {};
-
-              // initialize all values to 0
-              for (const key of value_keys) {
-                var tmp = {};
-                tmp.value = 0;
-                tmp.unit = message.values[key].unit;
-
-                result[message.values[key].name] = tmp;
-              }
-
-              for (const id in body.result) {
-                const set = body.result[id];
-
-                for (const key in set) {
-                  if ( message.values.hasOwnProperty(key) ) {
-                    const value = set[key];
-                    if (value) {
-                      for (const elm of value[message.id]) {
-                        if (elm.val) {
-                          var tmp = {};
-                          
-                          if (elm.val[0] && elm.val[0].tag) {
-                            tmp.value = elm.val[0].tag;
-                          }
-                          else {
-                            tmp.value = elm.val / message.values[key].divider;
-                            tmp.unit = message.values[key].unit;
-                          }
-
-                          result[message.values[key].name] = tmp;
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-
-              if (callback) {
-                callback(result);
-              }
-            }
-          }
-        }
-      );
-    }
-
-    function getFreeSessionsCount(node, callback) {
-      const url = buildUrl(node.use_tls, node.ip_address, "/dyn/sessionCheck.json");
-
-      node.debug("requesting " + url);
-
-      request(
-        url,
-        {},
-        (error, response, body) => {
-          node.debug("response to " + url + ": " + JSON.stringify(response));
-
-          if (error) {
-            node.error(error);
-          } else if (body) {
-            if (body.result && body.result.cntFreeSess != null) {
-              if (callback) {
-                callback(body.result.cntFreeSess);
-              }
-            } else {
-              node.log(body);
-            }
-          }
-        }
-      );
-    }
-
-    function logout(node, callback) {
-      if (node.ip_address){
-        const url = buildUrl(node.use_tls, node.ip_address, "/dyn/logout.json?sid=" + _sid[node.ip_address]);
-
-        node.log(url);
-
-        request(
-          url,
-          {},
-          (error, response, body) => {
-            node.debug("response to " + url + ": " + JSON.stringify(response));
-
-            var result = false;
-
-            if (error) {
-              node.error(error);
-            } else if (body) {
-              if (body.result && body.result.login != null) {
-                if (body.result.isLogin == false) {
-                  result = true;
-
-                  node.log("session closed: " + _sid[node.ip_address]);
-                  _sid[node.ip_address] = null;
-                }
-              } else {
-                node.log(body);
-              }
-            }
-
-            if (callback) {
-              callback(result);
-            }
-          }
-        );
-      }
-    }
-
-    function query(node, retries, completion) {
-      if (retries > 0) {
-        if (_sid[node.ip_address]) {
-          getValues(node, (result) => {
-            getFreeSessionsCount(node, (count) => {
-              result.available_sessions = count;
-
-              completion(result);
-            })
-          }, () => {
-            login(node, (sid) => {
-              query(node, retries - 1, completion);
-            });
+            completion(result);
           })
-        } else {
+        }, () => {
           login(node, (sid) => {
             query(node, retries - 1, completion);
           });
-        }
+        })
+      } else {
+        login(node, (sid) => {
+          query(node, retries - 1, completion);
+        });
       }
     }
+  }
 
-    function SMAWebconnectNode(config) {
-      RED.nodes.createNode(this, config);
-      this.ip_address = config.ip_address;
-      this.use_tls = config.use_tls;
-      this.right = config.right;
-      var node = this;
-      node.on("input", function(msg) {
-        message = msg.payload;
-        
-        query(node, 3, (result) => {
-          msg.payload = result;
-          node.send(msg);
-        });
-      });
-      node.on("close", function(done) {
-        logout(node, (result) => {
-          done();
-        });
-      });
-    }
-
-    RED.nodes.registerType("sma-webconnect", SMAWebconnectNode, {
-      credentials: {
-        password: { type: "password" }
+  function SMAWebconnectNode(config) {
+    RED.nodes.createNode(this, config);
+    this.ip_address = config.ip_address;
+    this.use_tls = config.use_tls;
+    this.right = config.right;			
+    this.device_selection = config.device_selection;
+    this.custom_message = {};
+    this.sid = null;
+    
+    var node = this;
+    node.on("input", function(msg) {
+      if (node.device_selection == "custom") {
+        node.custom_message = msg.payload;
       }
+
+      query(node, 3, (result) => {
+        msg.payload = result;
+        node.send(msg);
+      });
     });
+    node.on("close", function(done) {
+      logout(node, (result) => {
+        done();
+      });
+    });
+  }
+
+  RED.nodes.registerType("sma-webconnect", SMAWebconnectNode, {
+    credentials: {
+      password: { type: "password" }
+    }
+  });
 };
