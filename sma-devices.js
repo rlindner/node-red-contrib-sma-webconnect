b0afd8c478572cefa559db2d1cfc1cd2bd4d200a
HoLo85 <winter@magic.ms>
2021-02-13 14:22:12 +0100

    code cleanup, moved devices to separate file

---
+++ sma-devices.js
@@ -0,0 +1,81 @@
+module.exports.obj = {
+    sb_tripower: {
+        "id": "1",
+        "values": {
+            "6100_0046E500": {
+            "name": "phase1_voltage",
+            "divider": 100
+            },
+            "6100_0046E600": {
+            "name": "phase2_voltage",
+            "divider": 100
+            },
+            "6100_0046E700": {
+            "name": "phase3_voltage",
+            "divider": 100
+            },
+            "6100_40463600": {
+            "name": "grid_feedin",
+            "divider": 1
+            },
+            "6100_40463700": {
+            "name": "grid_consumption",
+            "divider": 1
+            },
+            "6100_40263F00": {
+            "name": "power",
+            "divider": 1
+            }
+        }
+    },
+
+    sb_storage: {
+        "id": "7",
+        "values": {
+            "6100_00295A00": {
+            "name": "charge_percent",
+            "divider": 1
+            },
+            "6100_00495C00": {
+            "name": "voltage",
+            "divider": 100
+            },
+            "6400_00496700": {
+            "name": "overall_charge_wh",
+            "divider": 1
+            },
+            "6100_00496900": {
+            "name": "current_charging_w",
+            "divider": 1
+            },
+            "6400_00497E00": {
+            "name": "current_charging_wh",
+            "divider": 1
+            },
+            "6400_00496800": {
+            "name": "overall_discharge_wh",
+            "divider": 1
+            },
+            "6100_00496A00": {
+            "name": "current_discharge_w",
+            "divider": 1
+            },
+            "6400_00496D00": {
+            "name": "current_discharge_wh",
+            "divider": 1
+            },
+            "6400_00496900": {
+            "name": "charge_w",
+            "divider": 1
+            },
+            "6100_40495B00": {
+            "name": "temperature_deg",
+            "divider": 10
+            },
+            "6180_08495E00": {
+            "name": "state_charging",
+            "divider": 1
+            }
+        }
+    }
+};
--- sma-webconnect.js
+++ sma-webconnect.js
@@ -1,85 +1,7 @@
+const sma_devices = require('./sma-devices').obj;
+
 module.exports = function (RED) {
   const retry = require("requestretry");
-
-  const sb_tripower = {
-    "id": "1",
-    "values": {
-      "6100_0046E500": {
-        "name": "phase1_voltage",
-        "divider": 100
-      },
-      "6100_0046E600": {
-        "name": "phase2_voltage",
-        "divider": 100
-      },
-      "6100_0046E700": {
-        "name": "phase3_voltage",
-        "divider": 100
-      },
-      "6100_40463600": {
-        "name": "grid_feedin",
-        "divider": 1
-      },
-      "6100_40463700": {
-        "name": "grid_consumption",
-        "divider": 1
-      },
-      "6100_40263F00": {
-        "name": "power",
-        "divider": 1
-      }
-    }
-  };
-
-  const sb_storage = {
-    "id": "7",
-    "values": {
-      "6100_00295A00": {
-        "name": "charge_percent",
-        "divider": 1
-      },
-      "6100_00495C00": {
-        "name": "voltage",
-        "divider": 100
-      },
-      "6400_00496700": {
-        "name": "overall_charge_wh",
-        "divider": 1
-      },
-      "6100_00496900": {
-        "name": "current_charging_w",
-        "divider": 1
-      },
-      "6400_00497E00": {
-        "name": "current_charging_wh",
-        "divider": 1
-      },
-      "6400_00496800": {
-        "name": "overall_discharge_wh",
-        "divider": 1
-      },
-      "6100_00496A00": {
-        "name": "current_discharge_w",
-        "divider": 1
-      },
-      "6400_00496D00": {
-        "name": "current_discharge_wh",
-        "divider": 1
-      },
-      "6400_00496900": {
-        "name": "charge_w",
-        "divider": 1
-      },
-      "6100_40495B00": {
-        "name": "temperature_deg",
-        "divider": 10
-      },
-      "6180_08495E00": {
-        "name": "state_charging",
-        "divider": 1
-      }
-    }
-  };
 
   var message = {};
 
@@ -147,7 +69,7 @@ module.exports = function (RED) {
       message = node.custom_config;
     }
     else {
-      message = eval(node.device_selection);
+      message = eval(sma_devices[node.device_selection]);
     }
 
     const value_keys = Object.keys(message.values);
